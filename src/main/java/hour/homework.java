9d41fc4aed5b6760d7dcf0adfae6a3e361d51793
Essmallwood <EsaylorSmallwood@wil.yearup.org>
2022-03-30 03:56:19 -0400

    Homework: 1 to 10 questions

---
+++ src/main/java/hour/homework.java
@@ -0,0 +1,129 @@
+/*
+
+//Question 1
+   public static int getLastIndex(String[] names) {
+        String result = names.length - 1;
+        }
+
+// Question 2
+    public static int getSecondToLastIndex(String[] names) {
+        int result = names.length - 2;
+        return result;
+    }
+
+// Question 3
+    public static String getFirstElement(String[] names) {
+        return names(0);
+    }
+
+
+//Question 4
+    public static String getLastElement(String[] names) {
+        for (int x = 0, x < names.length)
+    }
+
+        zString names = {"Emoni", "Jim", "Bobby", "Even"};
+       String result = names.length;
+    }
+
+ //Question 5
+    public static String getSecondToLastElement(String[] names) {
+        int total = 0;
+        for (int x = 0; x < names.length; x++) {
+            names[x];
+            return total;
+
+        }
+
+        public static void main (String[]args){
+
+            int[] ints = {1, 2, 3, 4, 5};
+            System.out.println(getSum(ints));
+        }
+    }
+
+ // Question 6
+    public static int getSum(int[] ints) {
+            int total = 0;
+            for (int x = 0; x < ints.length; x++) {
+                total = total + ints[x];
+              return total;
+
+            }
+
+            public static void main (String[]args){
+
+                int[] ints = {1, 2, 3, 4, 5};
+                System.out.println(getSum(ints));
+            }
+    }
+
+ // Question 7
+   public static int getAverage(int[] ints) {
+        int total = 0;
+        for (int x = 0; x < ints.length; x++) {
+            total = total + ints[x];
+            double result = total / ints.length;
+
+        }
+
+        public static void main (String[]args){
+
+            int[] array = {1, 2, 3, 4, 5};
+            System.out.println(getAverage(array));
+        }
+
+
+ // Question 8
+        public static String extractAllOddNumbers ( int[] numbers){
+            int x = 0;
+            for (int x; x < int.length; x++) {
+                if (numbers[x] % numbers [x] == 1) ;
+                {
+                    return numbers[x];
+                }
+            }
+            public static void main (String [] args) {
+                int [] numbers = {1,2,3,4,5,6,7,8,9,10};
+                System.out.println (extractAllOddNumbers ( int[] numbers));
+            }
+
+        }
+
+
+ // Question 9
+        public static String extractAllEvenNumbers ( int[] numbers){
+            int x = 0;
+            for (int x; x < int.length; x++) {
+                if (numbers[x] % 2 == 0) ;
+                {
+                    return numbers[x];
+                }
+            }
+                public static void main (String [] args) {
+                    int numbers [] = {1,2,3,4,5,6,7,8,9,10};
+                    System.out.println (extractAllEvenNumbers ( int[] numbers));
+                }
+
+        }
+
+
+
+
+ // Question 10
+    public static boolean contains(String[] names, String element) {
+        for (int x; x < names.length; x++) {
+            if (names[x].equals(element)) ;
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Emoni", "Jim", "Bob"};
+        String element = "Emoni";
+        System.out.println(contains(names, "Emoni"));
+    }
+}
+
+ */
